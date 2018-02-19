4d65d977c14fafbd17c789a7fd4c6045a36ba37e
Vitaly Dragun <vdrag00n90@gmail.com>
2018-02-19 15:09:40 +0200

    Handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://vitaly1990:19900225@ds243008.mlab.com:43008/storybooks-dev',
+    googleClientID: '718124641948-nev53169qdsvcfruin9eqj8reiulqe06.apps.googleusercontent.com',
+    googleClientSecret: 'jkNW3NsT2gdswalVjD8Y9YwW'
+};
