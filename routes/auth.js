438f33f3687b9ce19bdce1be67508a8b6fa99716
Vitaly Dragun <vdrag00n90@gmail.com>
2018-02-20 15:11:55 +0200

    Add google authentication to our app.

--- routes/auth.js
+++ routes/auth.js
@@ -6,9 +6,22 @@ router.get('/google', passport.authenticate('google', { scope: ['profile', 'emai
 
 router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }),
-     (req, res) => {
+    (req, res) => {
         // Successful authentication, redirect dashboard.
         res.redirect('/dashboard');
     });
+
+router.get('/verify', (req, res) => {
+   if (req.user) {
+       console.log(req.user);
+   }  else {
+       console.log('Not Auth');
+   }
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 
 module.exports = router;
